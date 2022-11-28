cfe303f1cb84de5ec841148e0557fe0cef6f7ae5
Lars Poetzel <lars.poetzel@gmail.com>
2022-11-28 17:04:10 +0100

    OnClick new quote

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import getActitivy from "./api";
+import axios from "axios";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const getActitivy = () => {
+    const response = axios
+      .get("https://www.boredapi.com/api/activity")
+      .then((response) => setPosts(response.data));
+    return response;
+  };
   useEffect(() => {
-    getActitivy().then((json) => {
-      setPosts(json);
-    });
+    getActitivy();
   }, []);
+
+  const getNewSuggestion = () => {
+    getActitivy();
+  };
+
   return (
     <>
       <p>{posts.activity}</p>
-      <button>New suggestion</button>
+      <button onClick={getNewSuggestion}>New suggestion</button>
     </>
   );
 }
