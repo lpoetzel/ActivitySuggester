c9eeead76383e818f425141c109f1e3f4917435a
Lars Poetzel <lars.poetzel@gmail.com>
2022-11-29 10:05:37 +0100

    max price

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,16 @@ import axios from "axios";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [price, setPrice] = useState();
   const getActitivy = () => {
     const response = axios
       .get("https://www.boredapi.com/api/activity")
+      .then((response) => setPosts(response.data));
+    return response;
+  };
+  const getMaxPrice = () => {
+    const response = axios
+      .get(`http://www.boredapi.com/api/activity?minprice=0&maxprice=${price}`)
       .then((response) => setPosts(response.data));
     return response;
   };
@@ -17,7 +24,6 @@ function App() {
   const getNewSuggestion = () => {
     getActitivy();
   };
-
   return (
     <>
       <div className="activity">{posts.activity}</div>
@@ -27,6 +33,16 @@ function App() {
           More information
         </a>
       ) : null}
+      <div>Price: {posts.price}</div>
+      <input
+        className="max-price"
+        type="text"
+        placeholder="e.g. 1.0"
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <button className="price-button" onClick={getMaxPrice}>
+        Max price
+      </button>
       <button className="new-button" onClick={getNewSuggestion}>
         New suggestion
       </button>
